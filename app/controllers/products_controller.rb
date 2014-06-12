2c6c78890fc552ea5bc4e0ed9dc2c04acb85c410
kennytrionfo <kentrionfo@gmail.com>
2014-06-12 14:22:27 -0600

    all

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,39 @@
 class ProductsController < ApplicationController
 
+  # The index action
+  # This should list all of our products
+  # index.html.erb
+
+  def index
+    @products = Product.all
+  end
+
+
+  #This should find a particular prodcut
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.create(product_params)
+    if @product.save
+    redirect_to products_path
+    else
+      #saving to the db field
+      #do something else
+      render :new
+    end
+  end
+
+  private
+  #define a provate method
+  #use strong parameters
+  def product_params
+    params.require(:product).permit(:name)
+  end
 
 end
